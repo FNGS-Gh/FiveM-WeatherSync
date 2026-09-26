a48fdf7ad9bf53af77693377f5b22d52803315a3
GhostQck <ghost3dolphin@gmail.com>
2026-09-26 12:45:08 +0400

    Updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Ghost'
 description 'Sync world weather among all players; Set live time updates; Define the in-game weather forecast.'
-version '1.1.0'
+version '1.1.1'
 
 client_script 'dist/client.js'
 server_script 'dist/server.js'
